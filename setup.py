971b0a70a488086f2e9e71637e6907195038c720
David Lerner <dmlerner@gmail.com>
2023-07-03 10:00:00 -0700

    version number again

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "ynab-api"
-VERSION = "2.0.1"
+VERSION = "2.0.2"
 # To install the library, run the following
 #
 # python setup.py install
